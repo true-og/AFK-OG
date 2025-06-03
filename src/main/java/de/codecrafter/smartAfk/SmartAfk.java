24b0cfa0bd83cc3530bc5b3a231f6f86bc32b403
Felix Heidisch <fh@heidischmal4.de>
2025-06-03 23:07:27 +0200

    Initial Commit

---
+++ src/main/java/de/codecrafter/smartAfk/SmartAfk.java
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the Minecraft Smart Afk project.
+ * Licensed under the MIT License. See the LICENSE file in the project root for full license information.
+ */
+
+package de.codecrafter.smartAfk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmartAfk extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
