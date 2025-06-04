f28366aa98c32ba1b2fd2877d02d9744ca9c50bc
Felix Heidisch <fh@heidischmal4.de>
2025-06-04 12:43:30 +0200

    Add afk location

--- src/main/java/de/codecrafter/smartAfk/utils/AfkManager.java
+++ src/main/java/de/codecrafter/smartAfk/utils/AfkManager.java
@@ -7,35 +7,45 @@ package de.codecrafter.smartAfk.utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class AfkManager {
     private final Set<UUID> afkPlayers = new HashSet<>();
+    private final Map<UUID, Location> afkPositions = new HashMap<>();
     private static final Component AFK_PREFIX = Component.text("[AFK] ", NamedTextColor.RED);
 
     public void setAfk(Player player) {
         afkPlayers.add(player.getUniqueId());
+        afkPositions.put(player.getUniqueId(), player.getLocation());
         player.setInvulnerable(true);
 
         Component name = player.name();
         player.displayName(AFK_PREFIX.append(name.color(NamedTextColor.WHITE)));
         player.playerListName(AFK_PREFIX.append(name.color(NamedTextColor.WHITE)));
+
+        player.sendMessage(Component.text("Du bist jetzt Afk.", NamedTextColor.GREEN));
     }
 
     public void unsetAfk(Player player) {
         afkPlayers.remove(player.getUniqueId());
+        afkPositions.remove(player.getUniqueId());
         player.setInvulnerable(false);
 
         Component name = player.name();
         player.displayName(name);
         player.playerListName(name);
+
+        player.sendMessage(Component.text("Du bist jetzt nicht mehr Afk.", NamedTextColor.YELLOW));
     }
 
     public boolean isAfk(Player player) {
         return afkPlayers.contains(player.getUniqueId());
+    }
+
+    public Location getAfkPosition(Player player) {
+        return afkPositions.get(player.getUniqueId());
     }
 }
